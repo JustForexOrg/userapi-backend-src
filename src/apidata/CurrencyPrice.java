f65009e3f0b9ca9c0e74904b04025177ee33cd9a
dsg115 <dsg115>
2017-06-07 18:30:42 +0100

    [dg][pnd] Refactored readfile to only iterate through once and for easier reusability

--- src/apidata/CurrencyPrice.java
+++ src/apidata/CurrencyPrice.java
@@ -8,14 +8,12 @@ public class CurrencyPrice {
     float getPrice(JFCurrency targetCurrency, JFCurrency baseCurrency, LocalDateTime t) {
         String year = String.valueOf(t.getYear());
         DateTime time  = new DateTime(t);
-        // open file as an array
-        // PARSE FILE!!!
         String filename = "_"+year+".json";
 
         if(baseCurrency == JFCurrency.USD) {
 
         } else {
-            filename = targetCurrency.toString() + filename;
+            filename = "src/stockData/" + targetCurrency.toString() + filename;
             // open file as array of arrays
             // search outer array for inner head of arrays & return the tail
         }
--- src/apidata/readFileTest.java
+++ src/apidata/readFileTest.java
@@ -10,41 +10,41 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class readFileTest {
+
+    private static List<Pair<String, Double>> p = new ArrayList<>();
+
     public static void main(String[] args) {
         String fileName = "src/stockData/test.json";
         fileName = System.getProperty("user.dir") + File.separator + fileName;
-        List<String> list = new ArrayList<>();
 
-        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-            list = stream
+        readFile(fileName);
+
+        for(Pair<String,Double> pair:p) {
+            System.out.println(pair.getElement0());
+            System.out.println(pair.getElement1());
+        }
+
+
+    }
+
+    private static void readFile(String filename) {
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            stream
                     .filter(line -> line.startsWith("[2"))
                     .map(readFileTest::removeComma)
                     .collect(Collectors.toList());
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        list.forEach(System.out::println);
-
-        String j = list.get(0);
-
-        List<Pair<String, Double>> l = makePair(list);
-    }
-
-    private static List<Pair<String, Double>> makePair(List<String> str) {
-        List<Pair<String, Double>> p = new ArrayList<>();
-        for (String s: str) {
-            String fst = s.substring(1, s.indexOf(","));
-            Double snd = Double.parseDouble(s.substring(s.indexOf(",")+1, s.length()-1));
-            System.out.println(fst);
-            System.out.println(snd);
-            p.add(new Pair<String, Double>(fst, snd));
-        }
-        return p;
     }
 
     private static String removeComma(String s) {
-        return (s.endsWith(",")) ? s.substring(0, s.length() - 1): s;
+        if (s.endsWith(",")) {
+             s = s.substring(0, s.length() - 1);
+        }
+        String fst = s.substring(1, s.indexOf(","));
+        Double snd = Double.parseDouble(s.substring(s.indexOf(",")+1, s.length()-1));
+        p.add(new Pair<>(fst, snd));
+        return s;
     }
 }
