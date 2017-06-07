f22b9913745a3386a42fc8e97cd0740f5d8b8230
dsg115 <dsg115>
2017-06-07 17:40:31 +0100

    [dg] Written skeleton code and fleshed out implementation of CurrencyPrice

--- src/apidata/CurrencyPrice.java
+++ src/apidata/CurrencyPrice.java
@@ -5,8 +5,21 @@ import utils.JFCurrency;
 import java.time.LocalDateTime;
 
 public class CurrencyPrice {
-    float getPrice(JFCurrency targetCurrency, JFCurrency baseCurrency, LocalDateTime time) {
-        return 0; //TODO
+    float getPrice(JFCurrency targetCurrency, JFCurrency baseCurrency, LocalDateTime t) {
+        String year = String.valueOf(t.getYear());
+        DateTime time  = new DateTime(t);
+        // open file as an array
+        // PARSE FILE!!!
+        String filename = "_"+year+".json";
+
+        if(baseCurrency == JFCurrency.USD) {
+
+        } else {
+            filename = targetCurrency.toString() + filename;
+            // open file as array of arrays
+            // search outer array for inner head of arrays & return the tail
+        }
+        return 0;
     }
 
 }
