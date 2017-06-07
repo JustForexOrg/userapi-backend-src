863a54e631116a9022b0bec35822662fec381580
dsg115 <dsg115>
2017-06-07 19:24:33 +0100

    [dg][pnd] Started implementation of getPrice

--- src/apidata/CurrencyPrice.java
+++ src/apidata/CurrencyPrice.java
@@ -2,9 +2,20 @@ package apidata;
 
 import utils.JFCurrency;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CurrencyPrice {
+
+    private static List<Pair<String, Double>> p = new ArrayList<>();
+
     float getPrice(JFCurrency targetCurrency, JFCurrency baseCurrency, LocalDateTime t) {
         String year = String.valueOf(t.getYear());
         DateTime time  = new DateTime(t);
@@ -13,11 +24,43 @@ public class CurrencyPrice {
         if(baseCurrency == JFCurrency.USD) {
 
         } else {
-            filename = "src/stockData/" + targetCurrency.toString() + filename;
+            filename = System.getProperty("user.dir") +
+                       File.separator +
+                       "src/stockData/" +
+                       targetCurrency.toString() +
+                       filename;
+
+            readFile(filename);
+
+            for(Pair<String,Double> pair:p) {
+                System.out.println(pair.getElement0());
+                System.out.println(pair.getElement1());
+            }
             // open file as array of arrays
             // search outer array for inner head of arrays & return the tail
         }
         return 0;
+    }
+
+    static void readFile(String filename) {
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            stream
+                    .filter(line -> line.startsWith("[2"))
+                    .map(CurrencyPrice::removeComma)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String removeComma(String s) {
+        if (s.endsWith(",")) {
+            s = s.substring(0, s.length() - 1);
+        }
+        String fst = s.substring(1, s.indexOf(","));
+        Double snd = Double.parseDouble(s.substring(s.indexOf(",")+1, s.length()-1));
+        p.add(new Pair<>(fst, snd));
+        return s;
     }
 
 }
--- src/apidata/readFileTest.java
+++ src/apidata/readFileTest.java
@@ -1,5 +1,7 @@
 package apidata;
 
+import utils.JFCurrency;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+
 public class readFileTest {
 
     private static List<Pair<String, Double>> p = new ArrayList<>();
@@ -17,34 +20,14 @@ public class readFileTest {
         String fileName = "src/stockData/test.json";
         fileName = System.getProperty("user.dir") + File.separator + fileName;
 
-        readFile(fileName);
+        CurrencyPrice.getPrice(JFCurrency.EUR, JFCurrency.USD, /* date */);
 
-        for(Pair<String,Double> pair:p) {
-            System.out.println(pair.getElement0());
-            System.out.println(pair.getElement1());
-        }
+        CurrencyPrice.
+
+//                test that currency price works
 
 
     }
 
-    private static void readFile(String filename) {
-        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
-            stream
-                    .filter(line -> line.startsWith("[2"))
-                    .map(readFileTest::removeComma)
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private static String removeComma(String s) {
-        if (s.endsWith(",")) {
-             s = s.substring(0, s.length() - 1);
-        }
-        String fst = s.substring(1, s.indexOf(","));
-        Double snd = Double.parseDouble(s.substring(s.indexOf(",")+1, s.length()-1));
-        p.add(new Pair<>(fst, snd));
-        return s;
-    }
 }
