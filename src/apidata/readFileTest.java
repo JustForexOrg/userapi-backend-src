a304acb39991eeab8fed217f09da8acfac69af59
dsg115 <dsg115>
2017-06-08 07:53:00 +0100

    [dg] Cleaned up readFileTest(ing) class and added more cases to check all normal data tests

--- src/apidata/readFileTest.java
+++ src/apidata/readFileTest.java
@@ -1,37 +1,19 @@
 package apidata;
 
 import utils.JFCurrency;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-
+// Testing file
 public class readFileTest {
 
-    private static List<Pair<String, Double>> p = new ArrayList<>();
-
     public static void main(String[] args) {
-        String fileName = "src/stockData/test.json";
-        fileName = System.getProperty("user.dir") + File.separator + fileName;
-
         LocalDateTime date = LocalDateTime.parse("2014-01-02T01:02:00");
+        LocalDateTime date2 = LocalDateTime.parse("2012-01-02T01:02:00");
 
-        CurrencyPrice.getPrice(JFCurrency.EUR, JFCurrency.USD, date);
-
-
-
-//                test that currency price works
-
-
+        System.out.println(CurrencyPrice.getPrice(JFCurrency.EUR, JFCurrency.USD, date));
+        System.out.println(CurrencyPrice.getPrice(JFCurrency.USD, JFCurrency.EUR, date2));
+        System.out.println(CurrencyPrice.getPrice(JFCurrency.EUR, JFCurrency.EUR, date));
+        System.out.println(CurrencyPrice.getPrice(JFCurrency.USD, JFCurrency.USD, date2));
     }
-
 
 }
